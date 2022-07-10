f5384426bc64ef647e070ff5a1a753a8102dace4
Samuli Seppänen <samuli@openvpn.net>
2022-07-10 15:41:01 +0300

    Rspec: add more elaborate tests for systemd Ubuntus
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- spec/classes/resolver_spec.rb
+++ spec/classes/resolver_spec.rb
@@ -3,17 +3,41 @@
 require 'spec_helper'
 
 describe 'resolver' do
-  default_params = { 'servers' => ['8.8.8.8', '8.8.4.4'] }
+  default_params = { 'servers' => ['8.8.8.8', '8.8.4.4'],
+                     'domains' => ['example.org', 'example.com'] }
+
+  xenial          = { supported_os: [{ 'operatingsystem' => 'Ubuntu', 'operatingsystemrelease' => ['16.04'] }] }
+  bionic          = { supported_os: [{ 'operatingsystem' => 'Ubuntu', 'operatingsystemrelease' => ['18.04'] }] }
+  focal           = { supported_os: [{ 'operatingsystem' => 'Ubuntu', 'operatingsystemrelease' => ['20.04'] }] }
+  jammy           = { supported_os: [{ 'operatingsystem' => 'Ubuntu', 'operatingsystemrelease' => ['22.04'] }] }
+  systemd_ubuntus = { supported_os: [{ 'operatingsystem' => 'Ubuntu', 'operatingsystemrelease' => ['18.04','20.04', '22.04'] }] }
+
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       extra_facts = {}
-      extra_facts[:lsbdistcodename] = 'RedHat' if os_facts[:osfamily] == 'RedHat'
-
+      extra_facts = { os: { distro: { codename: 'RedHat' } } } if os_facts[:osfamily] == 'RedHat'
       let(:facts) { os_facts.merge(extra_facts) }
       let(:params) { default_params }
 
       it { is_expected.to compile }
     end
   end
+
+  on_supported_os(systemd_ubuntus).each do |os, os_facts|
+    context "default resolver on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) { default_params }
+
+      it { is_expected.to contain_class('resolver::systemd_resolved') }
+      it { is_expected.to contain_file('/etc/systemd/resolved.conf.d') }
+      it { is_expected.to contain_file('/etc/systemd/resolved.conf.d/50_puppet_resolver.conf').with('notify'  => 'Exec[restart-systemd-resolved]',
+                                                                                                    'require' => 'File[/etc/systemd/resolved.conf.d]') }
+      it { is_expected.to contain_exec('restart-systemd-resolved').with('command'     => 'systemctl restart systemd-resolved',
+                                                                        'refreshonly' => true) }
+    end
+  end
+
+
+
 end
